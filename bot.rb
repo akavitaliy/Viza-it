89d97d828d070790458abdfd8f787968538c784a
VitamiN <scoundrel98@gmail.com>
2023-10-17 12:39:58 +0300

    init

--- bot.rb
+++ bot.rb
@@ -1,6 +1,6 @@
 require 'telegram/bot'
 
-@token = ''
+@token = '6395073876:AAFqb-fk2AMn6qoVC3jWiFVq9nZ-AFfxjWY'
 
 def bot_send(message)    
     Telegram::Bot::Client.run(@token) do |bot|
--- main.rb
+++ main.rb
@@ -11,48 +11,17 @@ browser = Ferrum::Browser.new(
     #extensions: ['stealth.min.js']    
 )
 
-accounts = [
-    { login: 'akavitamin_man22@rambler.ru',  password: '' },
-    { login: 'akavitamin_man21@rambler.ru',  password: '' },
-    { login: 'akavitamin_man20@rambler.ru',  password: '' },
-    { login: 'akavitamin_man19@rambler.ru',  password: '' },
-    { login: 'akavitamin_man18@rambler.ru',  password: '' },
-    { login: 'akavitamin_man17@rambler.ru',  password: '' },
-    { login: 'akavitamin_man16@rambler.ru',  password: '' },
-    { login: 'akavitamin_man15@rambler.ru',  password: '' },
-    { login: 'akavitamin_man14@rambler.ru',  password: '' },
-    { login: 'akavitamin_man13@rambler.ru',  password: '' },
-    { login: 'akavitamin_man12@rambler.ru',  password: '' },
-    { login: 'akavitamin_man11@rambler.ru',  password: '' },
-    { login: 'akavitamin_man10@rambler.ru',  password: '' },
-    { login: 'akavitamin_man9@rambler.ru',   password: '' },
-    { login: 'akavitamin_man8@rambler.ru',   password: '' },
-    { login: 'akavitamin_man7@rambler.ru',   password: '' },
-    { login: 'akavitamin_man6@rambler.ru',   password: '' },
-    { login: 'akavitamin_man5@rambler.ru',   password: '' },
-    { login: 'akavitamin_man4@rambler.ru',   password: '' },
-    { login: 'akavitamin_man3@rambler.ru',   password: '' },
-    { login: 'akavitamin_man2@rambler.ru',   password: '' },
-    { login: 'akavitamin_man1@rambler.ru',   password: '' },
-    { login: 'akavitamin_man@rambler.ru',    password: '' },
-    { login: 'akavitaminman4@gmail.com',     password: '' },
-    { login: 'akavitaminman3@gmail.com',     password: '' },
-    { login: 'akavitaminman2@gmail.com',     password: '' },
-    { login: 'akavitaminman1@gmail.com',     password: '' },
-    { login: 'vitaminr2test@gmail.com',      password: '' },
-    { login: 'scoundrel98@gmail.com',        password: '' },
-    { login: 'vitamin_man@mail.ru',          password: '' },
-  ]
-
+accounts = File.open("email.txt")
+password = '123456123aKa!'
 
 accounts.each do |account|
     6.times do
         start(browser)
         screenshot(browser)
         puts '=' * 20
-        puts account[:login] 
+        puts account 
         puts '=' * 20
-        login(browser, account[:login], account[:password])
+        login(browser, account, password)
         begin
             cheak_day(browser)
         rescue => exception
@@ -62,44 +31,3 @@ accounts.each do |account|
         end                      
     end
 end
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-# def parser(browser)
-#     begin
-#         start(browser)
-#     rescue => exception
-#         puts exception.message
-#         parser(browser)
-#     end
-
-#     screenshot(browser)
-
-#     login(browser, login, password)
-
-#     begin
-#         cheak_day(browser)
-#     rescue => exception
-#         puts exception.message.colorize(:red)
-#         puts 'Ошибка!'.colorize(:red) 
-#         gets       
-#         sleep 3
-#         parser(browser)
-#     end
-
-#     gets
-#     browser.quit
-# end
-
-#parser(browser)
